5f31992c6106178e692ac94a387d21e936f032fd
daniel <daniel.n96@t-online.de>
2017-11-09 07:48:56 +0100

    TODO buttonpressed method

--- src/sample/Controller9.java
+++ src/sample/Controller9.java
@@ -16,12 +16,40 @@ public class Controller9 {
 
     @FXML
     public Button b910_OneWorld,b911_Intro,b912_AGlobalSport,b913_AnInternetLifestyle,b914_PlayFair,b915_WorkingOnAProject,b916_FocusOnTechnologyAnsSociety;
-
+    public boolean b911 = false;
 
     String enableButton = "-fx-background-color: #e6f319";
 
 
     public void buttonPressed(ActionEvent actionEvent) {
+        Button btn = (Button)actionEvent.getSource();
+        String buttonId = btn.getId();
+
+        if(b911_Intro ==((Button) actionEvent.getSource())){
+            if(b911==false){
+                b911=true;
+                System.out.println("in");
+                //b911_Intro.setStyle(enableButton);
+
+                b911_Intro.getStylesheets().add("/data/css/test.css");
+
+                // b911_Intro.setStyle(t);
+            }else{
+                b911 = false;
+                b911_Intro.getStylesheets().remove(0);
+                b911_Intro.getStylesheets().add("/data/css/class.css");
+            }
+        }
+
+
+        //Get the pressed Button and change it color
+        //((Button) actionEvent.getSource()).setStyle(enableButton);
+
+
+
+        System.out.println(buttonId);
     }
+
+
 
 }
